1cd9d392b97122bd58da1e2284a8317406a7b42f
Dether D <Dether-dether@yandex.ru>
2021-12-12 17:12:38 +0300

    urls

--- ChatRepository/ChatContext.cs
+++ ChatRepository/ChatContext.cs
@@ -6,7 +6,7 @@ namespace ChatRepository
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("server=217.25.89.68;database=drag;user=sDether;password=1namQfeg1_;");
+            optionsBuilder.UseMySQL("server=localhost;database=drag;user=sDether;password=1namQfeg1_;");
             base.OnConfiguring(optionsBuilder);
         }
 
