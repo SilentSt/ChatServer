b599b0132ec40f267cd5d52e964b93cbafd3db21
Dether D <Dether-dether@yandex.ru>
2021-12-12 22:51:30 +0300

    await

--- Chat Server/Repository/UserRepository.cs
+++ Chat Server/Repository/UserRepository.cs
@@ -18,26 +18,26 @@ namespace Chat_Server.Repository
 
         public async Task AddToken(User user, string token)
         {
-            chatContext.Tokens.Add(new Tokens() { Token = token, UserId = user.Id });
+            await chatContext.Tokens.AddAsync(new Tokens() { Token = token, UserId = user.Id });
             await chatContext.SaveChangesAsync();
         }
 
         public async Task AddToken(int userid, string token)
         {
             var user = await GetUser(userid);
-            chatContext.Tokens.Add(new Tokens() { Token = token, UserId = user.Id });
+            await chatContext.Tokens.AddAsync(new Tokens() { Token = token, UserId = user.Id });
             await chatContext.SaveChangesAsync();
         }
 
         public async Task<User> FindUser(string nickname)
         {
-            var user = chatContext.Users.FirstOrDefault(u => u.NickName.Contains(nickname));
+            var user = await chatContext.Users.FirstOrDefaultAsync(u => u.NickName.Contains(nickname));
             return user ?? new User() { Id = 0 };
         }
 
         public async Task<List<User>> FindUsers(string nickname)
         {
-            var users = chatContext.Users.Where(u => u.NickName.Contains(nickname)).ToList();
+            var users = await chatContext.Users.Where(u => u.NickName.Contains(nickname)).ToListAsync();
             return users;
         }
 
@@ -45,7 +45,7 @@ namespace Chat_Server.Repository
         {
             if (id < 0)
             {
-                var users = chatContext.Users.Where(c => c.CompanyId == id).ToList();
+                var users = await chatContext.Users.Where(c => c.CompanyId == id).ToListAsync();
                 return users;
             }
             return new();
@@ -61,18 +61,18 @@ namespace Chat_Server.Repository
 
         public async Task<User> GetUser(int id)
         {
-            var user = chatContext.Users.FirstOrDefault(u => u.Id == id);
+            var user = await chatContext.Users.FirstOrDefaultAsync(u => u.Id == id);
             return user ?? new User() { Id = 0 };
         }
         public async Task<User> GetUser(string token)
         {
-            var user = chatContext.Users.FirstOrDefault(x => x.Tokens.Any(y => y.Token == token));
+            var user = await chatContext.Users.FirstOrDefaultAsync(x => x.Tokens.Any(y => y.Token == token));
             return user ?? new User() { Id = 0 };
         }
 
         public async Task<User> Login(string username, string password)
         {
-            var user = chatContext.Users.FirstOrDefault(u => u.Login == username && u.CompanyId < 0 && u.Password == password);
+            var user = await chatContext.Users.FirstOrDefaultAsync(u => u.Login == username && u.CompanyId < 0 && u.Password == password);
             return user ?? new User() { Id = 0 };
         }
     }
