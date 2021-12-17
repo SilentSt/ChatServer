b854d7f9b590143a1ce16d58a15eeee9f82cbd1a
sDether <Dether-dether@yandex.ru>
2021-12-17 22:19:37 +0300

    message lower json

--- ChatRepository/Message.cs
+++ ChatRepository/Message.cs
@@ -12,13 +12,18 @@ namespace ChatRepository
     public class Message
     {
         [Key]
+        [JsonProperty("id")]
         public int Id { get; set; }
         [Required]
+        [JsonProperty("fromid")]
         public int FromId { get; set; }
         [Required]
+        [JsonProperty("chatid")]
         public int ChatId { get; set; }
         [Required]
+        [JsonProperty("utctime")]
         public DateTime UtcTime { get; set; }
+        [JsonProperty("reply")]
         public string? Reply { get; set; }
     }
 }
