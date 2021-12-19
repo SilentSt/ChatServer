cf94743edb700fbdd20a202e4aa1ec54f2ebb8da
sDether <Dether-dether@yandex.ru>
2021-12-19 21:39:55 +0300

    format

--- Chat Server/Controllers/BoardController.cs
+++ Chat Server/Controllers/BoardController.cs
@@ -8,129 +8,130 @@ using Newtonsoft.Json.Linq;
 
 namespace Chat_Server.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class BoardController : ControllerBase
+  [Route("[controller]")]
+  [ApiController]
+  public class BoardController : ControllerBase
+  {
+    private IBoardRepository boarddata;
+    IUserRepository userdata;
+    public BoardController(BoardRepository bdata, UserRepository udata)
     {
-        private IBoardRepository boarddata;
-        IUserRepository userdata;
-        public BoardController(BoardRepository bdata, UserRepository udata)
-        {
-            boarddata = bdata;
-            userdata = udata;
-        }
-
-        [HttpPost("board")]
-        public async Task<IActionResult> CreateBoard([FromBody] CreateBoard board)
-        {
-            try
-            {
-                var user = await userdata.GetUser(board.token);
-                long boardid;
-                if (board.priv)
-                {
-                    boardid = await boarddata.CreateBoard(user.Id, board.title);
-                }
-                else
-                {
-                    boardid = await boarddata.CreateBoard(user.CompanyId, board.title);
-                }
-
-                if (boardid == 0) return NotFound();
-                return new ContentResult() { Content = boardid.ToString(), StatusCode = 200 };
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Console.WriteLine(e.Message);
-                return BadRequest();
-            }
-        }
-
-        [HttpPost("getboard")]
-        public async Task<IActionResult> GetBoard(GetBoard board)
-        {
-            try
-            {
-                if ((await userdata.GetUser(board.token)).Id == 0) return NotFound();
-                var resboard = await boarddata.GetBoard(board.boardid);
-                return new ContentResult() { Content = JObject.FromObject(resboard).ToString(), StatusCode = 200 };
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Console.WriteLine(e.Message);
-                return BadRequest();
-            }
-        }
-
-        [HttpPost("updateboard")]
-        public async Task<IActionResult> UpdateBoard([FromBody] UpdateBoard board)
-        {
-            try
-            {
-                if ((await userdata.GetUser(board.token)).Id == 0) return NotFound();
-                await boarddata.UpdateBoard(board.boardid, board.title);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Console.WriteLine(e.Message);
-                return BadRequest();
-            }
-        }
-
-        [HttpPost("card")]
-        public async Task<IActionResult> CreateCard([FromBody] CreateCard card)
-        {
-            try
-            {
-                if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
-                var id = await boarddata.CreateCard(card.boardid, card.title, card.description, card.state,
-                    card.deadline);
-                if (id == 0) return BadRequest();
-                return new ContentResult(){Content = id.ToString(),StatusCode = 200};
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Console.WriteLine(e.Message);
-                return BadRequest();
-            }
-        }
-
-        [HttpPost("deletecard")]
-        public async Task<IActionResult> DeleteCard([FromBody] DeleteCard card)
-        {
-            try
-            {
-                if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
-                await boarddata.DeleteCard(card.cardid);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                return BadRequest();
-            }
-        }
-
-        [HttpPost("updatecard")]
-        public async Task<IActionResult> UpdateCard([FromBody]UpdateCard card)
-        {
-            try
-            {
-                if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
-                await boarddata.UpdateCard(card.cardid, card.title, card.description, card.state, card.deadline);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Console.WriteLine(e.Message);
-                return BadRequest();
-            }
-        }
-
+      boarddata = bdata;
+      userdata = udata;
     }
+
+    [HttpPost("board")]
+    public async Task<IActionResult> CreateBoard([FromBody] CreateBoard board)
+    {
+      try
+      {
+        var user = await userdata.GetUser(board.token);
+        long boardid;
+        if (board.priv)
+        {
+          boardid = await boarddata.CreateBoard(user.Id, board.title);
+        }
+        else
+        {
+          boardid = await boarddata.CreateBoard(user.CompanyId, board.title);
+        }
+
+        if (boardid == 0) return NotFound();
+        return new ContentResult() { Content = boardid.ToString(), StatusCode = 200 };
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        Console.WriteLine(e.Message);
+        return BadRequest();
+      }
+    }
+
+    [HttpPost("getboard")]
+    public async Task<IActionResult> GetBoard(GetBoard board)
+    {
+      try
+      {
+        if ((await userdata.GetUser(board.token)).Id == 0) return NotFound();
+        var resboard = await boarddata.GetBoard(board.boardid);
+        return new ContentResult() { Content = JObject.FromObject(resboard).ToString(), StatusCode = 200 };
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        Console.WriteLine(e.Message);
+        return BadRequest();
+      }
+    }
+
+    [HttpPost("updateboard")]
+    public async Task<IActionResult> UpdateBoard([FromBody] UpdateBoard board)
+    {
+      try
+      {
+        if ((await userdata.GetUser(board.token)).Id == 0) return NotFound();
+        await boarddata.UpdateBoard(board.boardid, board.title);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        Console.WriteLine(e.Message);
+        return BadRequest();
+      }
+    }
+
+    [HttpPost("card")]
+    public async Task<IActionResult> CreateCard([FromBody] CreateCard card)
+    {
+      try
+      {
+        if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
+        var id = await boarddata.CreateCard(card.boardid, card.title, card.description, card.state,
+            card.deadline);
+        if (id == 0) return BadRequest();
+        return new ContentResult() { Content = id.ToString(), StatusCode = 200 };
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.ToString());
+        Console.WriteLine(e.Message);
+        return BadRequest();
+      }
+    }
+
+    [HttpPost("deletecard")]
+    public async Task<IActionResult> DeleteCard([FromBody] DeleteCard card)
+    {
+      try
+      {
+        if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
+        await boarddata.DeleteCard(card.cardid);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        return BadRequest();
+      }
+    }
+
+    [HttpPost("updatecard")]
+    public async Task<IActionResult> UpdateCard([FromBody] UpdateCard card)
+    {
+      try
+      {
+        if ((await userdata.GetUser(card.token)).Id == 0) return NotFound();
+        await boarddata.UpdateCard(card.cardid, card.title, card.description, card.state, card.deadline);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+
+        Console.WriteLine(e.ToString());
+        Console.WriteLine(e.Message);
+        return BadRequest();
+      }
+    }
+
+  }
 }
--- Chat Server/Repository/MessageRepository.cs
+++ Chat Server/Repository/MessageRepository.cs
@@ -10,90 +10,91 @@ using Newtonsoft.Json.Linq;
 
 namespace Chat_Server.Repository
 {
-    public class MessageRepository : IMessageRepository
+  public class MessageRepository : IMessageRepository
+  {
+    ChatContext chatContext;
+
+
+    public MessageRepository(ChatContext chat)
     {
-        ChatContext chatContext;
-
-        public MessageRepository(ChatContext chat)
-        {
-            chatContext = chat;
-        }
-
-        public async Task<long> CreateChat(int userid, string name)
-        {
-            var chatid = Generator.Generate64Id();
-            await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Name = name, Private = false });
-            await chatContext.SaveChangesAsync();
-            return chatid;
-        }
-
-        public async Task<long> CreatePrivateChat(int userid, int friendid)
-        {
-            var chatid = Generator.Generate64Id();
-            await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Private = true });
-            await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = friendid, Private = true });
-            await chatContext.SaveChangesAsync();
-            return chatid;
-        }
-
-        public async Task<List<Message>> GetChatMessages(int userid, int chatid, int skip = 0, int take = 25)
-        {
-            if (chatContext.Chats.Any(c => c.UserId == userid && c.ChatId == chatid))
-            {
-                var messages = await chatContext.History.Where(c => c.ChatId == chatid).OrderBy(f => f.UtcTime).ToListAsync();
-                return messages;
-            }
-            throw new Exception("403");
-        }
-
-        public async Task<List<UserChats>> GetChats(int userid)
-        {
-            var chats = await chatContext.Chats.Where(c => c.UserId == userid).ToListAsync();
-            List<UserChats> rchats = new List<UserChats>();
-            foreach (var chat in chats)
-            {
-                rchats.Add(
-                    new UserChats()
-                    {
-                        chatid = chat.ChatId,
-                        name = chat.Name,
-                        users = chatContext.Chats.Where(g => g.ChatId == chat.ChatId).Select(x => new ComUser()
-                        {
-                            nick = chatContext.Users.First(t => t.Id == x.UserId).NickName,
-                            id = x.UserId
-                        }).ToList(),
-                        priv = chat.Private
-                    });
-            }
-            return rchats;
-        }
-
-        public async Task<List<Message>> GetMessages(int userid, long chatid, int skip = 0, int take = 25)
-        {
-            
-            var messages = chatContext.History.Where(y => y.ChatId == chatid).OrderByDescending(d=>d.UtcTime).Skip(skip).Take(take);
-           
-            return await messages.ToListAsync();
-        }
-
-        public async Task AddUserToChat(int userid, long chatid)
-        {
-            var chatname = await GetChatName(chatid);
-            await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Private = false, Name = chatname });
-            await chatContext.SaveChangesAsync();
-        }
-
-        public async Task<int> SendMessage(int fromid, long toid, string text, string? reply = null)
-        {
-            var message = await chatContext.History.AddAsync(new Message()
-            { FromId = fromid, ChatId = toid,Text = text, UtcTime = DateTime.UtcNow, Reply = reply });
-            await chatContext.SaveChangesAsync();
-            return message.Entity.Id;
-        }
-
-        private async Task<string?> GetChatName(long id)
-        {
-            return (await chatContext.Chats.FirstOrDefaultAsync(x => x.ChatId == id)).Name;
-        }
+      chatContext = chat;
     }
+
+    public async Task<long> CreateChat(int userid, string name)
+    {
+      var chatid = Generator.Generate64Id();
+      await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Name = name, Private = false });
+      await chatContext.SaveChangesAsync();
+      return chatid;
+    }
+
+    public async Task<long> CreatePrivateChat(int userid, int friendid)
+    {
+      var chatid = Generator.Generate64Id();
+      await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Private = true });
+      await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = friendid, Private = true });
+      await chatContext.SaveChangesAsync();
+      return chatid;
+    }
+
+    public async Task<List<Message>> GetChatMessages(int userid, int chatid, int skip = 0, int take = 25)
+    {
+      if (chatContext.Chats.Any(c => c.UserId == userid && c.ChatId == chatid))
+      {
+        var messages = await chatContext.History.Where(c => c.ChatId == chatid).OrderBy(f => f.UtcTime).ToListAsync();
+        return messages;
+      }
+      throw new Exception("403");
+    }
+
+    public async Task<List<UserChats>> GetChats(int userid)
+    {
+      var chats = await chatContext.Chats.Where(c => c.UserId == userid).ToListAsync();
+      List<UserChats> rchats = new List<UserChats>();
+      foreach (var chat in chats)
+      {
+        rchats.Add(
+            new UserChats()
+            {
+              chatid = chat.ChatId,
+              name = chat.Name,
+              users = chatContext.Chats.Where(g => g.ChatId == chat.ChatId).Select(x => new ComUser()
+              {
+                nick = chatContext.Users.First(t => t.Id == x.UserId).NickName,
+                id = x.UserId
+              }).ToList(),
+              priv = chat.Private
+            });
+      }
+      return rchats;
+    }
+
+    public async Task<List<Message>> GetMessages(int userid, long chatid, int skip = 0, int take = 25)
+    {
+
+      var messages = chatContext.History.Where(y => y.ChatId == chatid).OrderByDescending(d => d.UtcTime).Skip(skip).Take(take);
+
+      return await messages.ToListAsync();
+    }
+
+    public async Task AddUserToChat(int userid, long chatid)
+    {
+      var chatname = await GetChatName(chatid);
+      await chatContext.Chats.AddAsync(new Chat() { ChatId = chatid, UserId = userid, Private = false, Name = chatname });
+      await chatContext.SaveChangesAsync();
+    }
+
+    public async Task<int> SendMessage(int fromid, long toid, string text, string? reply = null)
+    {
+      var message = await chatContext.History.AddAsync(new Message()
+      { FromId = fromid, ChatId = toid, Text = text, UtcTime = DateTime.UtcNow, Reply = reply });
+      await chatContext.SaveChangesAsync();
+      return message.Entity.Id;
+    }
+
+    private async Task<string?> GetChatName(long id)
+    {
+      return (await chatContext.Chats.FirstOrDefaultAsync(x => x.ChatId == id)).Name;
+    }
+  }
 }
